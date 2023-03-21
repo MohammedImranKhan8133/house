64daef0122a66a5167abe2bdd3cdaae507af553e
mohammed imran <mohammedimran1939543@gmail.com>
2023-03-21 15:32:35 +0530

    spring class added into master branch.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,22 @@
 package org.example;
 
 public class Main {
+    private int id;
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         String name = "imran";
         int a = 1;
     }
+
+    public int getsId() {
+        return id;
+    }
+
+    public void setsId(int id) {
+        this.id = id;
+    }
+
     public void method() {
         System.out.println("github");
     }

0612b625ab9c324ec4a57b103b8c532bb3e32cba
mohammed imran <mohammedimran1939543@gmail.com>
2023-03-21 11:21:57 +0530

    super message added

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -3,14 +3,21 @@ package org.example;
 public class Student {
     private int id;
     private String name;
-    
+
     private float salary;
     private double amount;
-    public void method(){
+
+    public void method() {
     }
-    public String name(){
+
+    public String name() {
         return "imran";
     }
-    public int id(){
+
+    public int id() {
+        System.out.println("super..");
         return 2;
+    }
+
+
 }
